047c20edc70fd288ddcb4008d6854c335f5f6d59
国进 颜 <guojin_yjs@cumt.edu.cn>
2022-10-04 12:56:00 +0800

    代码修改

--- openvino_deploy_pp-yoloe/openvino_deploy_pp-yoloe.cpp
+++ openvino_deploy_pp-yoloe/openvino_deploy_pp-yoloe.cpp
@@ -37,32 +37,34 @@ void openvino_deploy_ppyoloe() {
 
     // 创建推理通道
     Predictor predictor(model_path, device_name);
-
+    // 创建数据处理类
     ImageProcess image_pro;
-
+    // 读取类别文件
     image_pro.read_class_names(lable_path);
-
+    // 图片预处理
     cv::Mat image = cv::imread(image_path);
-
     cv::Size input_size(640, 640);
+    // 将图片放到方形背景中
     int length = image.rows > image.cols ? image.rows : image.cols;
     cv::Mat input_mat = cv::Mat::zeros(length, length,CV_8UC3);
     cv::Rect roi(0, 0, image.cols, image.rows);
-
     image.copyTo(input_mat(roi));
 
+    // 设置缩放比例
     image_pro.scale_factor = (double)length / 640.0 ;
 
+    // 归一化处理
     cv::Mat input_data = image_pro.image_normalize(input_mat, input_size, 2);
 
+    // 加载模型推理数据
     ov::Tensor input_tensor = predictor.get_tensor(input__node_name);
-
     predictor.fill_tensor_data_image(input_tensor, input_data);
+    // 模型推理
     predictor.infer();
-    std::cout << "Hello World!\n";
+    // 读取推理结果
     std::vector<float> result_boxes = predictor.get_output_data(output_box_node_name);
     std::vector<float> result_conf = predictor.get_output_data(output_conf_node_name);
-    std::cout << "Hello World!\n";
+    // 处理推理结果
     cv::Mat result_image = image_pro.yoloe_result_process(image, result_boxes, result_conf);
 
     cv::imshow("result", result_image);
